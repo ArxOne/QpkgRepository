cf26ba3133095b276ff7e6db5c9358d29b437d7c
picrap <pascal@craponne.fr>
2024-01-08 18:06:42 +0100

    Handling partial qpkg fileset (ignoring it)

--- QpkgRepository/QpkgRepository.cs
+++ QpkgRepository/QpkgRepository.cs
@@ -130,6 +130,7 @@ public class QpkgRepository
                     packages.Add(loadPackagesFromSource);
                 }
                 catch (FormatException) { }
+                catch (FileNotFoundException) { }
             }
         }
 
