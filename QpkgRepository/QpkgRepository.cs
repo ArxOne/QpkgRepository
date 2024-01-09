2857510ca4f9dc4d829aca51e832364d4ba92ea8
picrap <pascal@craponne.fr>
2024-01-09 10:57:14 +0100

    Generating XDocument instead of XmlDocument

--- QpkgRepository/QpkgRepository.cs
+++ QpkgRepository/QpkgRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace ArxOne.Qnap;
+﻿using System.Xml.Linq;
+
+namespace ArxOne.Qnap;
 
 using System;
 using System.Collections.Generic;
@@ -27,70 +29,64 @@ public class QpkgRepository
             source.Cache = null;
     }
 
-    public XmlDocument GetXml(params string[]? model) => GetXml(model, null);
+    public XDocument GetXml(Func<string, Version?>? onVersionFailed) => GetXml(null, onVersionFailed);
 
-    public XmlDocument GetXml(Func<string, Version?>? onVersionFailed) => GetXml(null, onVersionFailed);
-
-    public XmlDocument GetXml(string[]? model, Func<string, Version?>? onVersionFailed = null)
+    public XDocument GetXml(string[]? model, Func<string, Version?>? onVersionFailed = null)
     {
-        var platforms = model is not null && model.Length > 0 ? model.Select(x => x.Replace(" ", "+")).ToList() : GetPlatforms();
+        var platforms = model is { Length: > 0 } ? model.Select(x => x.Replace(" ", "+")).ToImmutableArray() : DefaultPlatforms;
         var packagesBysSource = LoadPackagesBySource(onVersionFailed);
 
-        var repository = new XmlDocument();
-        var plugins = repository.CreateElement("plugins");
-        repository.AppendChild(plugins);
+        var latestPackages = packagesBysSource.GroupBy(x => x.Name).Select(g => g.MaxBy(p => p.Version));
+        var itemElements = latestPackages.Select(p => CreateItemElement(p, platforms));
 
-        var cacheCheck = repository.CreateElement("cachechk");
-        cacheCheck.InnerText = DateTime.Now.ToString("yyyyMMddHHmm");
-        plugins.AppendChild(cacheCheck);
-
-        foreach (var packages in packagesBysSource.GroupBy(x => x.Name))
-        {
-            var package = packages.MaxBy(x => x.Version);
-
-            if (package is null)
-                continue;
-
-            var item = repository.CreateElement("item");
-            plugins.AppendChild(item);
-
-            repository.AddElementCData(item, "name", package.DisplayName);
-            repository.AddElement(item, "internalName", package.Name);
-            repository.AddElementCData(item, "description", package.Summary);
-            repository.AddElement(item, "version", package.Version.ToString());
-            repository.AddElementCData(item, "maintainer", package.Author);
-            repository.AddElementCData(item, "developer", package.Author);
-
-
-            foreach (var platformId in platforms)
-            {
-                var platform = repository.CreateAndAddElement(item, "platform");
-                repository.AddElement(platform, "platformID", platformId);
-                repository.AddElement(platform, "location", package.Location.AbsoluteUri);
-                if (!string.IsNullOrEmpty(package.Signature))
-                    repository.AddElement(platform, "signature", package.Signature);
-            }
-
-            repository.AddElement(item, "category", package.Category);
-            repository.AddElement(item, "type", package.Type);
-            repository.AddElement(item, "changeLog", package.ChangelogLink);
-            repository.AddElement(item, "publishedDate", package.PublishedDate.ToString("yyyy/MM/dd"));
-            repository.AddElement(item, "language", package.Languages);
-            repository.AddElement(item, "icon80", package.Icon80Uri);
-            repository.AddElement(item, "icon100", package.Icon100Uri);
-            if (package.SnapshotUri is not null)
-                repository.AddElementCData(item, "snapshot", package.SnapshotUri);
-
-            repository.AddElementCData(item, "forumLink", package.ForumLink);
-            repository.AddElementCData(item, "bannerImg", package.BannerImg);
-            repository.AddElementCData(item, "tutorialLink", package.TutorialLink);
-            repository.AddElement(item, "fwVersion", package.FirmwareMinimumVersion);
-
-        }
-        return repository;
+        var plugins = new XElement("plugins",
+            new XElement("cachechk", DateTime.Now.ToString("yyyyMMddHHmm"))
+        );
+        plugins.Add(itemElements);
+        return new XDocument(plugins);
     }
 
-    private List<QpkgPackage> LoadPackagesBySource(Func<string, Version?>? onVersionFailed = null)
+    private XElement CreateItemElement(QpkgPackage package, IEnumerable<string> platforms)
+    {
+        var itemElement = new XElement("item",
+            new XElement("name", package.DisplayName),
+            new XElement("internalName", package.Name),
+            new XElement("description", package.Summary),
+            new XElement("version", package.Version.ToString()),
+            new XElement("maintainer", package.Author),
+            new XElement("developer", package.Author)
+            );
+        itemElement.Add(platforms.Select(p => CreatePlatformElement(package, p)));
+        itemElement.Add(
+            new XElement("category", package.Category),
+            new XElement("type", package.Type),
+            new XElement("changeLog", package.ChangelogLink),
+            new XElement("publishedDate", package.PublishedDate.ToString("yyyy/MM/dd")),
+            new XElement("language", package.Languages),
+            new XElement("icon80", package.Icon80Uri),
+            new XElement("icon100", package.Icon100Uri)
+        );
+        if (package.SnapshotUri is not null)
+            itemElement.Add(new XElement("snapshot", new XCData(package.SnapshotUri)));
+        itemElement.Add(
+            new XElement("forumLink", new XCData(package.ForumLink)),
+            new XElement("bannerImg", new XCData(package.BannerImg)),
+            new XElement("tutorialLink", new XCData(package.TutorialLink)),
+            new XElement("fwVersion", package.FirmwareMinimumVersion)
+        );
+        return itemElement;
+    }
+
+    private XElement CreatePlatformElement(QpkgPackage package, string platform)
+    {
+        return new XElement("platform",
+            new XElement("platformID", platform),
+            new XElement("location", package.Location.AbsoluteUri),
+            new XElement("signature", package.Signature)
+        );
+    }
+
+    private IEnumerable<QpkgPackage> LoadPackagesBySource(Func<string, Version?>? onVersionFailed = null)
     {
         var packagesBySource = new List<QpkgPackage>();
         foreach (var source in _sources)
@@ -101,7 +97,7 @@ public class QpkgRepository
         return packagesBySource;
     }
 
-    private List<QpkgPackage> LoadPackagesFromSource(IList<string> files, QpkgRepositorySource source, Func<string, Version?>? onVersionFailed = null)
+    private IEnumerable<QpkgPackage> LoadPackagesFromSource(IList<string> files, QpkgRepositorySource source, Func<string, Version?>? onVersionFailed = null)
     {
         var packages = new List<QpkgPackage>();
 
@@ -186,125 +182,124 @@ public class QpkgRepository
         JsonSerializer.Serialize(cacheWriter, repositoryCache);
     }
 
-
-    private static List<string> GetPlatforms() => new List<string>
-    {
-            "HS-251",
-            "HS-251+",
-            "HS-251D",
-            "MiroKing",
-            "Mustang-200",
-            "QB-103",
-            "QGD-1600",
-            "QGD-1602",
-            "QGD-3014",
-            "SS-X53",
-            "TNC-X51B",
-            "TS-1232XU",
-            "TS-1635AX",
-            "TS-1685",
-            "TS-231P2",
-            "TS-231P3",
-            "TS-251+",
-            "TS-431P",
-            "TS-431P2",
-            "TS-431P3",
-            "TS-451+",
-            "TS-531P",
-            "TS-673A",
-            "TS-832X",
-            "TS-EC1080 Pro",
-            "TS-EC1280U",
-            "TS-EC1280U R2",
-            "TS-EC1280U-RP",
-            "TS-EC1680U",
-            "TS-EC1680U R2",
-            "TS-EC1680U-RP",
-            "TS-EC2480U",
-            "TS-EC2480U R2",
-            "TS-EC2480U-RP",
-            "TS-EC880 Pro",
-            "TS-EC880U",
-            "TS-EC880U R2",
-            "TS-EC880U-RP",
-            "TS-i410X",
-            "TS-KVM",
-            "TS-NASARM",
-            "TS-NASX86",
-            "TS-X16",
-            "TS-X28A",
-            "TS-X31P2",
-            "TS-X31P3",
-            "TS-X31X",
-            "TS-X31XU",
-            "TS-X32",
-            "TS-X32U",
-            "TS-X33",
-            "TS-X35",
-            "TS-X35A",
-            "TS-X35EU",
-            "TS-X41",
-            "TS-X51",
-            "TS-X51+",
-            "TS-X51A",
-            "TS-X51AU",
-            "TS-X51B",
-            "TS-X51D",
-            "TS-X51DU",
-            "TS-X51U",
-            "TS-X53",
-            "TS-X53B",
-            "TS-X53BU",
-            "TS-X53D",
-            "TS-X53E",
-            "TS-X53II",
-            "TS-X53U",
-            "TS-X63",
-            "TS-X63U",
-            "TS-X64",
-            "TS-X64U",
-            "TS-X71",
-            "TS-X71U",
-            "TS-X72",
-            "TS-X72U",
-            "TS-X73",
-            "TS-X73A",
-            "TS-X73AU",
-            "TS-X73U",
-            "TS-X74",
-            "TS-X75",
-            "TS-X75U",
-            "TS-X77",
-            "TS-X77U",
-            "TS-X79U",
-            "TS-X80",
-            "TS-X80U",
-            "TS-X82",
-            "TS-X82S",
-            "TS-X82U",
-            "TS-X83XU",
-            "TS-X85",
-            "TS-X85U",
-            "TS-X87XU",
-            "TS-X88",
-            "TS-X88U",
-            "TS-X89FU",
-            "TS-X89U",
-            "TS-X90",
-            "TS-X90U",
-            "TS-XA28A",
-            "TS-XA51",
-            "TS-XA73",
-            "TS-XA82",
-            "TS-XA83XU",
-            "TVS-463",
-            "TVS-663",
-            "TVS-672X",
-            "TVS-672XT",
-            "TVS-863",
-            "TVS-863+",
-            "TVS-872X",
-            "TVS-872XT",
-            "TVS-882ST3"
-    };
+    private static readonly IReadOnlyList<string> DefaultPlatforms =
+    [
+        "HS-251",
+        "HS-251+",
+        "HS-251D",
+        "MiroKing",
+        "Mustang-200",
+        "QB-103",
+        "QGD-1600",
+        "QGD-1602",
+        "QGD-3014",
+        "SS-X53",
+        "TNC-X51B",
+        "TS-1232XU",
+        "TS-1635AX",
+        "TS-1685",
+        "TS-231P2",
+        "TS-231P3",
+        "TS-251+",
+        "TS-431P",
+        "TS-431P2",
+        "TS-431P3",
+        "TS-451+",
+        "TS-531P",
+        "TS-673A",
+        "TS-832X",
+        "TS-EC1080 Pro",
+        "TS-EC1280U",
+        "TS-EC1280U R2",
+        "TS-EC1280U-RP",
+        "TS-EC1680U",
+        "TS-EC1680U R2",
+        "TS-EC1680U-RP",
+        "TS-EC2480U",
+        "TS-EC2480U R2",
+        "TS-EC2480U-RP",
+        "TS-EC880 Pro",
+        "TS-EC880U",
+        "TS-EC880U R2",
+        "TS-EC880U-RP",
+        "TS-i410X",
+        "TS-KVM",
+        "TS-NASARM",
+        "TS-NASX86",
+        "TS-X16",
+        "TS-X28A",
+        "TS-X31P2",
+        "TS-X31P3",
+        "TS-X31X",
+        "TS-X31XU",
+        "TS-X32",
+        "TS-X32U",
+        "TS-X33",
+        "TS-X35",
+        "TS-X35A",
+        "TS-X35EU",
+        "TS-X41",
+        "TS-X51",
+        "TS-X51+",
+        "TS-X51A",
+        "TS-X51AU",
+        "TS-X51B",
+        "TS-X51D",
+        "TS-X51DU",
+        "TS-X51U",
+        "TS-X53",
+        "TS-X53B",
+        "TS-X53BU",
+        "TS-X53D",
+        "TS-X53E",
+        "TS-X53II",
+        "TS-X53U",
+        "TS-X63",
+        "TS-X63U",
+        "TS-X64",
+        "TS-X64U",
+        "TS-X71",
+        "TS-X71U",
+        "TS-X72",
+        "TS-X72U",
+        "TS-X73",
+        "TS-X73A",
+        "TS-X73AU",
+        "TS-X73U",
+        "TS-X74",
+        "TS-X75",
+        "TS-X75U",
+        "TS-X77",
+        "TS-X77U",
+        "TS-X79U",
+        "TS-X80",
+        "TS-X80U",
+        "TS-X82",
+        "TS-X82S",
+        "TS-X82U",
+        "TS-X83XU",
+        "TS-X85",
+        "TS-X85U",
+        "TS-X87XU",
+        "TS-X88",
+        "TS-X88U",
+        "TS-X89FU",
+        "TS-X89U",
+        "TS-X90",
+        "TS-X90U",
+        "TS-XA28A",
+        "TS-XA51",
+        "TS-XA73",
+        "TS-XA82",
+        "TS-XA83XU",
+        "TVS-463",
+        "TVS-663",
+        "TVS-672X",
+        "TVS-672XT",
+        "TVS-863",
+        "TVS-863+",
+        "TVS-872X",
+        "TVS-872XT",
+        "TVS-882ST3"
+    ];
 }
